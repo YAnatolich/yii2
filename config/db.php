b4fb3a55eda4d9d7a27268728628c950b95a6873
YAnatolich <ja.kalugin@gmail.com>
2016-02-18 10:43:56 +0200

    add config/db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=tz',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=test1.my:8080;dbname=tz',
+    'username' => 'root2',
+    'password' => 'NtvysqGfk',
     'charset' => 'utf8',
 ];
